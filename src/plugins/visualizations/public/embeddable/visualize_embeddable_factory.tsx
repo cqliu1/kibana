51ae1ae797e5dd69315b462c37ab91aed480bcef
Catherine Liu <catherine.liu@elastic.co>
2021-03-09 09:31:20 -0700

    Moved panel toolbar into presentation_util
    Fixed ts errors
    
    Fixed tsconfig
    
    Fixed quick button background color

--- src/plugins/visualizations/public/embeddable/visualize_embeddable_factory.tsx
+++ src/plugins/visualizations/public/embeddable/visualize_embeddable_factory.tsx
@@ -50,7 +50,7 @@ interface VisualizationAttributes extends SavedObjectAttributes {
 
 export interface VisualizeEmbeddableFactoryDeps {
   start: StartServicesGetter<
-    Pick<VisualizationsStartDeps, 'inspector' | 'embeddable' | 'dashboard' | 'savedObjectsClient'>
+    Pick<VisualizationsStartDeps, 'inspector' | 'embeddable' | 'savedObjectsClient'>
   >;
 }
 
@@ -154,7 +154,10 @@ export class VisualizeEmbeddableFactory
     }
   }
 
-  public async create(input: VisualizeInput & { savedVis?: SerializedVis }, parent?: IContainer) {
+  public async create(
+    input: VisualizeInput & { savedVis?: SerializedVis; newVisType?: string },
+    parent?: IContainer
+  ) {
     // TODO: This is a bit of a hack to preserve the original functionality. Ideally we will clean this up
     // to allow for in place creation of visualizations without having to navigate away to a new URL.
     if (input.savedVis) {
@@ -173,6 +176,7 @@ export class VisualizeEmbeddableFactory
       showNewVisModal({
         originatingApp: await this.getCurrentAppId(),
         outsideVisualizeApp: true,
+        newVisType: input?.newVisType,
       });
       return undefined;
     }
